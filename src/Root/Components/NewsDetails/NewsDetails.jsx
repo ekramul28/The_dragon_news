35e67a45292a848aab60ef939953323363e97321
Ekramul <mdekramulhassan168@gmail.com>
2023-10-04 15:57:08 +0600

    news detelas page added

--- src/Root/Components/NewsDetails/NewsDetails.jsx
+++ src/Root/Components/NewsDetails/NewsDetails.jsx
@@ -1,6 +1,9 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import Header from "../Header/Header";
 import HeaderNav from "../HeaderNav/HeaderNav";
+import FindUs from "../Find us/FindUs";
+import Qzone from "../Qzone/Qzone";
+import LoginWithGoogle from "../LoginWithGoogle/LoginWithGoogle";
 
 const NewsDetails = () => {
     const { id } = useParams();
@@ -12,6 +15,21 @@ const NewsDetails = () => {
         <div>
             <Header></Header>
             <HeaderNav></HeaderNav>
+            <div className="grid md:grid-cols-2 lg:grid-cols-4">
+                <div className="col-span-3">
+                    <div className="bg-[#fff] p-3">
+                        <img className="w-full mb-4" src={news.image_url} alt="" />
+                        <p className="font-bold text-2xl mb-5">{news.title}</p>
+                        <p>{news.details}</p>
+                        <button className="btn bg-[#D72050] border-none text-white rounded-none mt-8">All news in this category</button>
+                    </div>
+                </div>
+                <div className="col-span-1">
+                    <LoginWithGoogle></LoginWithGoogle>
+                    <FindUs></FindUs>
+                    <Qzone></Qzone>
+                </div>
+            </div>
         </div>
     );
 };
